9f5dc6a2bcb943bca4b46c458a5da5bc05856a97
ke-pan <ke_pan@yeah.net>
2016-06-07 08:25:09 +0800

    feat

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,16 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :name,
+             :description,
+             :location,
+             :open_at,
+             :close_at,
+             :category,
+             :score,
+             :pic_url
+
+  has_many :rates
+
+  def pic_url
+    object.picture.url
+  end
 end
