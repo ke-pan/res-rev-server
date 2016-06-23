b62b821c4cae25d5f2a5fa2397bfa55d49a3294a
ke-pan <ke_pan@yeah.net>
2016-06-24 07:39:10 +0800

    fix: n+1

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.includes(:rates).all
     render json: @restaurants
   end
 
