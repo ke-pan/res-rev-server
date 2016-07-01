cb3070940ac3ad7df4bb696a284c2cd3098b1015
ke-pan <ke_pan@yeah.net>
2016-07-01 22:10:14 +0800

    feat: create a rate

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -6,8 +6,26 @@ class RatesController < ApplicationController
     render json: @rates, include: :user
   end
 
+  def create
+    rate = @restaurant.rates.build(rate_attributes.merge(user_id: current_user.id))
+    if rate.save
+      render json: rate, status: :created, location: [@restaurant, rate]
+    else
+      respond_errors rate, :unprocessable_entity
+    end
+  end
+
   private
+
   def find_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def rate_attributes
+    rate_params[:attributes] || {}
+  end
+
+  def rate_params
+    params.require(:data).permit(:type, attributes: [:score, :comment])
   end
 end
--- test/controllers/rates_controller_test.rb
+++ test/controllers/rates_controller_test.rb
@@ -10,4 +10,19 @@ class RatesControllerTest < ActionDispatch::IntegrationTest
     get restaurant_rates_path(@restaurant)
     assert_response :success
   end
+
+  test 'create' do
+    assert_difference('Rate.count', 1) do
+      post restaurant_rates_path(@restaurant),
+        params: {
+          data: {
+            type: 'rates',
+            attributes: {
+              score: 4,
+              comment: 'good'
+            }
+          }
+        }
+    end
+  end
 end
